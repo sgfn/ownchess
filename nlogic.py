f8ad226ad8efd3dc2db070b98d077610642e78d3
sgfn <jpeesarek@gmail.com>
2022-02-17 21:36:34 +0100

    Implement further (incomplete) move legality checks

--- nlogic.py
+++ nlogic.py
@@ -17,6 +17,8 @@ class Square:
         self._piece = piece
     
     def __str__(self) -> str:
+        """Return the piece in FEN for displaying on the board."""
+
         if self._colour == 'e':
             return ' '
         else:
@@ -64,19 +66,19 @@ class Board:
         self._chessboard = [Square() for _ in range(64)]
 
         # Create variables to hold board properties
-        self._to_move = None
-        self._can_castle = None
+        self._to_move = '-'
+        self._can_castle = '-'
         self._ep_square = -1
-        self._halfmove_clock = None
-        self._fullmove_counter = None
+        self._halfmove_clock = -1
+        self._fullmove_counter = -1
 
         # # Create piece lists
         # self._white_pieces = []
         # self._black_pieces = []
         
-        # # Create helper variables containing the king's positions
-        # self._white_king_coords_tpl = None
-        # self._black_king_coords_tpl = None
+        # Create helper properties containing the king's positions
+        self._w_king_sq = -1
+        self._b_king_sq = -1
 
     def __str__(self, highlit_squares=set()) -> str:
         """
@@ -121,10 +123,19 @@ class Board:
         if fen == '':
             fen = Board.FEN_DEF
 
+        # Clear the board
+        for square in self._chessboard:
+            square.set_sq()
+
         # Set the starting properties
-        (rows, self._to_move, self._can_castle, self._ep_square, 
-        self._halfmove_clock, self._fullmove_counter) = fen.strip().split(' ')
+        (rows, self._to_move, self._can_castle, ep_sq, hm_cl, 
+        fm_ct) = fen.strip().split(' ')
         
+        # Convert the values
+        self._ep_square = self.alg_to_num(ep_sq)
+        self._halfmove_clock = int(hm_cl)
+        self._fullmove_counter = int(fm_ct)
+
         # Create the pieces and insert them on the chessboard
         for f_row, row_str in enumerate(rows.split('/')):
             col = 0
@@ -137,9 +148,10 @@ class Board:
                 else:
                     colour = 'b' if char.lower() == char else 'w'
                     piece = char.lower()
-                        
-                    self._chessboard[(7-f_row)*8 + col].set_sq(colour, piece)
-
+                    sq_num = (7-f_row)*8 + col
+                    if piece == 'k':
+                        self._update_king(colour, sq_num)
+                    self._chessboard[sq_num].set_sq(colour, piece)
                     col += 1
         
     def alg_to_num(self, coords_str: str) -> int:
@@ -147,24 +159,54 @@ class Board:
         Converts algebraic coordinates of a single square to numerical ones 
         used by the board.
         """
-
+        if len(coords_str) != 2:
+            return -1
         rank, file_ord = int(coords_str[1]), ord(coords_str[0].lower())
         # file 'a' -> col 0, ord('a') = 97
         return (rank - 1) * 8 + (file_ord - 97)
 
-    def move_piece(self, from_num: int, to_num: int = -1) -> None:
-        """Moves/removes a single piece (numerical coordinates)."""
-
+    def move_piece(self, from_num: int, to_num: int = -1, 
+                   promote_to: str = 'q') -> int:
+        """
+        Moves/removes a single piece (numerical coordinates). Returns 1 (pawn 
+        move), 2 (capture) if move resets the halfmove counter; 0 otherwise.
+        """
+        returnval = 0
         from_sq = self._chessboard[from_num]
 
         if from_sq._colour == 'e':
             print(f'DEBUG: Square {from_num} is empty')
             return None
 
+        if from_sq._piece == 'k':
+            self._update_king(from_sq._colour, to_num)
+        # Not checking if target square contains a king, might be needed later
+
+        if to_num != -1:
+            if self._chessboard[to_num]._colour not in ('e', from_sq._colour):
+                returnval = 2
+
+        if from_sq._piece == 'p':
+            # Handling promotions
+            # DON'T WORK AS INTENDED RIGHT NOW
+            pr_row = 7 if from_sq._colour == 'w' else 0
+            if to_num // 8 == pr_row:
+                self.add_piece(from_sq._colour, promote_to, to_num)
+                to_num = -1
+            
+            # Handling en passant
+            elif to_num == self._ep_square:
+                if to_num > from_num:
+                    self._chessboard[to_num - 8].set_sq()
+                else:
+                    self._chessboard[to_num + 8].set_sq()
+            returnval = 1
+
         if to_num != -1:
             self._chessboard[to_num].set_sq(from_sq._colour, from_sq._piece)
-
         from_sq.set_sq()
+
+        return returnval
 
     def add_piece(self, colour: str, piece: str, sq_num: int) -> None:
         """Adds a piece at the specified coordinates (numerical)."""
@@ -173,38 +215,41 @@ class Board:
         if to_sq._colour != 'e':
             print(f'DEBUG: Square {sq_num} is occupied, replacing')
 
+        if piece == 'k':
+            self._update_king(colour, sq_num)
+
         to_sq.set_sq(colour, piece)
 
     def get_pseudolegal_moves(self, sq_num: int) -> List[int]:
         """
         Returns a list of squares available for pseudolegal moves.
         Does not take checks into account.
-        Castling, en passant TBA.
+        Castling TBA.
         """
-        
         from_sq = self._chessboard[sq_num]
+        from_row, from_col = sq_num // 8, sq_num % 8
         pseudolegal_moves = []
         all_moves = []
 
         if from_sq._colour == 'e':
             print(f'DEBUG: Square {sq_num} is empty')
-
+        
         # Piece is a pawn
         elif from_sq._piece == 'p':
-            start_row, pawn_move = (1, 8) if from_sq._colour == 'w' else (6, -8)
-            
+            start_row, pawn_move = (1, 1) if from_sq._colour == 'w' else (6, -1)
             # Standard move
-            all_moves.append(pawn_move)
+            all_moves.append((pawn_move, 0))
             # First pawn move
-            if 8*start_row <= sq_num <= 8*start_row + 7:
-                all_moves.append(2*pawn_move)
+            if from_row == start_row:
+                all_moves.append((2*pawn_move, 0))
             # Pawn captures
-            all_captures = [pawn_move - 1, pawn_move + 1]
+            all_captures = [(pawn_move, -1), (pawn_move, 1)]
 
-            for mv_num in all_moves:
-                dest_num = sq_num + mv_num
+            for mv_row, mv_col in all_moves:
+                dest_row, dest_col = from_row + mv_row, from_col + mv_col
                 # Checking this might be redundant
-                if 0 <= dest_num <= 63:
+                if 0 <= dest_row <= 7 and 0 <= dest_col <= 7:
+                    dest_num = dest_row*8 + dest_col
                     to_sq = self._chessboard[dest_num]
                     # Cannot advance pawns onto occupied squares
                     if to_sq._colour != 'e':
@@ -212,46 +257,45 @@ class Board:
                     # If square is empty, the pawn can be moved there
                     pseudolegal_moves.append(dest_num)
 
-            for mv_num in all_captures:
-                dest_num = sq_num + mv_num
+            for mv_row, mv_col in all_captures:
+                dest_row, dest_col = from_row + mv_row, from_col + mv_col
                 # See above
-                if 0 <= dest_num <= 63:
+                if 0 <= dest_row <= 7 and 0 <= dest_col <= 7:
+                    dest_num = dest_row*8 + dest_col
                     to_sq = self._chessboard[dest_num]
                     # Standard pawn capture, en passant capture
                     if ((to_sq._colour not in ('e', from_sq._colour)) or 
                         (to_sq._colour == 'e' and self._ep_square == dest_num)):
                         pseudolegal_moves.append(dest_num)
 
-            return pseudolegal_moves
-
         # Piece is a king or a knight (not a ray piece)
         elif from_sq._piece in ('k', 'n'):
             if from_sq._piece == 'k':
-                all_moves = [-9, -8, -7, -1, 1, 7, 8, 9]
+                all_moves = [(1, 1), (1, 0), (1, -1), (0, 1),
+                             (0, -1), (-1, 1), (-1, 0), (-1, -1)]
             else:
-                # NOT A GOOD IDEA
-                all_moves = [-17, -15, -10, -6, 6, 10, 15, 17]
+                all_moves = [(1, 2), (1, -2), (-1, 2), (-1, -2),
+                             (2, 1), (2, -1), (-2, 1), (-2, -1)]
 
-            for mv_num in all_moves:
-                dest_num = sq_num + mv_num
-                if 0 <= dest_num <= 63:
+            for mv_row, mv_col in all_moves:
+                dest_row, dest_col = from_row + mv_row, from_col + mv_col
+                if 0 <= dest_row <= 7 and 0 <= dest_col <= 7:
+                    dest_num = dest_row*8 + dest_col
                     to_sq = self._chessboard[dest_num]
                     if to_sq._colour != from_sq._colour:
                         pseudolegal_moves.append(dest_num)
 
-            return pseudolegal_moves
-
         # Piece is a bishop, a rook or a queen (ray piece)
         else:
             if from_sq._piece in ('b', 'q'):
-                all_moves = [-9, -7, 7, 9]
+                all_moves = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
             if from_sq._piece in ('r', 'q'):
-                all_moves.extend((-8, -1, 1, 8))
+                all_moves.extend(((-1, 0), (0, -1), (1, 0), (0, 1)))
 
-            for mv_num in all_moves:
-                i = 1
-                dest_num = sq_num + i*mv_num
-                while 0 <= dest_num <= 63:
+            for mv_row, mv_col in all_moves:
+                dest_row, dest_col = from_row + mv_row, from_col + mv_col
+                while 0 <= dest_row <= 7 and 0 <= dest_col <= 7:
+                    dest_num = dest_row*8 + dest_col
                     to_sq = self._chessboard[dest_num]
                     if to_sq._colour == 'e':
                         pseudolegal_moves.append(dest_num)
@@ -259,42 +303,154 @@ class Board:
                         if to_sq._colour != from_sq._colour:
                             pseudolegal_moves.append(dest_num)
                         break
-                    i += 1
-                    dest_num = sq_num + i*mv_num
+                    dest_row += mv_row
+                    dest_col += mv_col
 
         return pseudolegal_moves
 
-    def show_pseudolegal_moves(self, sq_num: int) -> None:
-        """Prints the output of get_pseudolegal_moves() to stdout."""
+    def show_legal_moves(self, sq_num: int) -> None:
+        """Prints the output of get_legal_moves() to stdout."""
+        legal_moves = self.get_legal_moves(sq_num)
+        print(self.__str__(highlit_squares=set(legal_moves)))
 
-        pseudolegal_moves = self.get_pseudolegal_moves(sq_num)
-        print(self.__str__(highlit_squares=set(pseudolegal_moves)))
-
-    def is_in_check(self, player_colour: str = '') -> bool:
+    def is_in_check(self, king_colour: str = '') -> bool:
         """
         Test whether the specified player (default: next to move) is in check.
         """
-        if player_colour == '':
-            player_colour = self._to_move
+        if king_colour == '':
+            king_colour = self._to_move
 
-        if player_colour == 'w':
-            k_row, k_col = self._white_king_coords_tpl
+        if king_colour == 'w':
+            k_row, k_col = self._w_king_sq // 8, self._w_king_sq % 8
         else:
-            k_row, k_col = self._black_king_coords_tpl
+            k_row, k_col = self._b_king_sq // 8, self._b_king_sq % 8
 
-        knight_fields = ((1, 2), (1, -2), (-1, 2), (-1, -2), 
-                         (2, 1), (2, -1), (-2, 1), (-2, -1))
-        
-        for mv_row, mv_col in knight_fields:
-            dest_row, dest_col = k_row + mv_row, k_col + mv_col
-            if 0 <= dest_row <= 7 and 0 <= dest_col <= 7:
-                piece = self._chessboard[dest_row][dest_col]
-                if (piece is not None and piece.type == 'n' 
-                    and piece.colour != player_colour):
+        # Pawn checks
+        pawn_move = 1 if king_colour == 'w' else -1
+        pawn_moves = [(pawn_move, -1), (pawn_move, 1)]
+        for mv_row, mv_col in pawn_moves:
+            atk_row, atk_col = k_row + mv_row, k_col + mv_col
+            if 0 <= atk_row <= 7 and 0 <= atk_col <= 7:
+                atk_sq = self._chessboard[atk_row*8 + atk_col]
+                if (atk_sq._colour != king_colour and atk_sq._piece == 'p'):
+                    return True
+
+        # "King checks" - illegal moves where both kings are on adjacent squares
+        king_moves = [(1, 1), (1, 0), (1, -1), (0, 1),
+                       (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+        for mv_row, mv_col in king_moves:
+            atk_row, atk_col = k_row + mv_row, k_col + mv_col
+            if 0 <= atk_row <= 7 and 0 <= atk_col <= 7:
+                atk_sq = self._chessboard[atk_row*8 + atk_col]
+                if (atk_sq._colour != king_colour and atk_sq._piece == 'k'):
+                    return True
+
+        # Knight checks
+        knight_moves = [(1, 2), (1, -2), (-1, 2), (-1, -2), 
+                         (2, 1), (2, -1), (-2, 1), (-2, -1)]
+        for mv_row, mv_col in knight_moves:
+            atk_row, atk_col = k_row + mv_row, k_col + mv_col
+            if 0 <= atk_row <= 7 and 0 <= atk_col <= 7:
+                atk_sq = self._chessboard[atk_row*8 + atk_col]
+                if (atk_sq._colour != king_colour and atk_sq._piece == 'n'):
                     return True
         
+        # Bishop, rook and queen (ray piece) checks
+        brq_moves = [(-1, -1, 'bq'), (-1, 1, 'bq'), (1, -1, 'bq'), (1, 1, 'bq'),
+                     (-1, 0, 'rq'), (0, -1, 'rq'), (1, 0, 'rq'), (0, 1, 'rq')]
+        for mv_row, mv_col, p_str in brq_moves:
+            atk_row, atk_col = k_row + mv_row, k_col + mv_col
+            while 0 <= atk_row <= 7 and 0 <= atk_col <= 7:
+                atk_num = atk_row*8 + atk_col
+                atk_sq = self._chessboard[atk_num]
+                if (atk_sq._colour != king_colour and atk_sq._piece in p_str):
+                    return True
+                elif atk_sq._colour != 'e':
+                    break
+                atk_row += mv_row
+                atk_col += mv_col
 
+        return False
+      
+    def _update_king(self, colour: str, sq_num: int) -> None:
+        """Update variables containing information about king positions."""
         
+        if colour == 'b':
+            self._b_king_sq = sq_num
+        else:
+            self._w_king_sq = sq_num
+
+    def get_legal_moves(self, from_num: int) -> List[int]:
+        """
+        Returns a list of squares available for legal moves.
+        Castling TBA.
+        """
+
+        pseudolegal_moves = self.get_pseudolegal_moves(from_num)
+        legal_moves = []
+        from_sq = self._chessboard[from_num]
+        from_colour = from_sq._colour
+        from_piece = from_sq._piece
+
+        if from_colour != self._to_move:
+            return []
+
+        for to_num in pseudolegal_moves:
+            to_sq = self._chessboard[to_num]
+            to_piece = to_sq._piece
+            self.move_piece(from_num, to_num)
+            if not self.is_in_check(from_colour):
+                legal_moves.append(to_num)
+            if to_piece != 'e':               
+                from_sq.set_sq(from_colour, from_piece)
+                to_sq.set_sq('b' if from_colour == 'w' else 'w', to_piece)
+            else:
+                # Handling en passant
+                if from_piece == 'p' and to_num == self._ep_square:
+                    if to_num > from_num:
+                        self._chessboard[to_num - 8].set_sq('b' if 
+                        from_colour == 'w' else 'w', to_piece)
+                    else:
+                        self._chessboard[to_num + 8].set_sq('b' if 
+                        from_colour == 'w' else 'w', to_piece)
+                self.move_piece(to_num, from_num)
+        
+        return legal_moves
+
+    def make_move(self, from_num: int, to_num: int, 
+                  promote_to: str = 'q') -> None:
+        """
+        Make a permanent move. Increments the halfmove clock as well as
+        the fullmove counter, changes the en passant target square.
+        Removal of castling rights TBA.
+        """
+        legal_moves = self.get_legal_moves(from_num)
+        if to_num not in legal_moves:
+            print("DEBUG: Illegal move")
+            return None
+        
+        # Detecting possibility of en passant in next ply
+        if self._chessboard[from_num]._piece == 'p':
+            if to_num - from_num == 16:
+                self._ep_square = to_num - 8
+            elif to_num - from_num == -16:
+                self._ep_square = to_num + 8
+            else:
+                self._ep_square = -1
+        else:
+            self._ep_square = -1
+
+        mp_returnval = self.move_piece(from_num, to_num, promote_to)
+
+        if mp_returnval > 0:
+            self._halfmove_clock = 0
+        else:
+            self._halfmove_clock += 1
+
+        if self._to_move == 'b':
+            self._fullmove_counter += 1
+
+        self._to_move = 'b' if self._to_move == 'w' else 'w'
 
 if __name__ == '__main__':
     board = Board()
@@ -307,14 +463,21 @@ if __name__ == '__main__':
             print(board)
         elif cmd in ('ad', 'a'):
             board.add_piece(*args)
-        elif cmd in ('mv', 'm'):
-            board.move_piece(*args)
+        elif cmd in ('mm', 'm'):
+            args = [int(arg) if arg not in 'bnrq' else arg for arg in args]
+            board.make_move(*args)
         elif cmd in ('slm', 's'):
-            board.show_pseudolegal_moves(int(*args))
+            board.show_legal_moves(board.alg_to_num(*args))
+        elif cmd in ('slmn', 'sn'):
+            board.show_legal_moves(int(*args))
         elif cmd in ('ff', 'fen', 'f'):
             fen_str = ''
             for arg in args:
                 fen_str += arg + ' '
             board.import_fen(fen_str) if fen_str != '' else board.import_fen()
+        elif cmd in ('i', 'iic'):
+            print(board.is_in_check(*args))
+        elif cmd in ('ant', 'num'):
+            print(board.alg_to_num(*args))
         else:
             print('DEBUG: Unknown command')
